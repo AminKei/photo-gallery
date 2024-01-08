ec734e73aadbf9bb3f35abb2b9b0a4ce8bf0ad00
AminKei <moakeimasi@gmail.com>
2024-01-08 20:04:49 +0330

    add project to git and complte other components

---
+++ src/Animation1.tsx
@@ -0,0 +1,366 @@
+import React from "react";
+import "./Animation1.css";
+
+function Animation1() {
+  return (
+
+    <div className="external">
+      <div className="horizontal-scroll-wrapper" >
+        <div className="img-wrapper slower">
+          <a
+            href="https://altphotos.com/photo/stylish-parisian-cafe-terrace-279/"
+            target="_blank"
+            rel="noopener"
+          >
+            <img
+              src="https://history-biography.com/wp-content/uploads/2022/12/julian-alvarez.webp"
+              alt=""
+            />
+
+            <h3>Mohamad amin Keimasi</h3>
+            <p>Front End Developer</p>
+
+            <div className="div-profile">
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-512.png"
+                alt=""
+              />
+              <img
+                src="https://cdn.icon-icons.com/icons2/2248/PNG/512/linkedin_icon_135436.png"
+                alt=""
+              />
+              <img
+                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                alt=""
+              />
+            </div>
+          </a>
+        </div>
+
+        <div className="img-wrapper faster">
+          <a
+            href="https://altphotos.com/photo/retro-boy-doll-wearing-elegant-clothes-330/"
+            target="_blank"
+            rel="noopener"
+          >
+            <img
+              src="https://d3mm2s9r15iqcv.cloudfront.net/en/wp-content/uploads/2022/10/backend-developers-keep-everything-moving.jpeg"
+              alt=""
+            />
+            <h3> Darvin Nunez Martinez</h3>
+            <p>Back End Developer</p>
+            <div className="div-profile">
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-512.png"
+                alt=""
+              />
+              <img
+                src="https://cdn.icon-icons.com/icons2/2248/PNG/512/linkedin_icon_135436.png"
+                alt=""
+              />
+              <img
+                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                alt=""
+              />
+            </div>
+          </a>
+        </div>
+
+        <div className="img-wrapper slower vertical">
+          <a
+            href="https://altphotos.com/photo/clocks-shop-exposition-window-reflecting-the-streets-277/"
+            target="_blank"
+            rel="noopener"
+          >
+            <img
+              src="https://d3j2s6hdd6a7rg.cloudfront.net/v2/uploads/media/default/0002/22/thumb_121585_default_news_size_5.jpeg"
+              alt=""
+            />
+            <h3> Virjil van dijk</h3>
+            <p>UIUX Desiner</p>
+            <div className="div-profile">
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-512.png"
+                alt=""
+              />
+              <img
+                src="https://cdn.icon-icons.com/icons2/2248/PNG/512/linkedin_icon_135436.png"
+                alt=""
+              />
+              <img
+                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                alt=""
+              />
+            </div>
+          </a>
+        </div>
+
+        <div className="img-wrapper slower slower-down">
+          <a
+            href="https://altphotos.com/photo/swans-and-ducks-swimming-in-a-river-264/"
+            target="_blank"
+            rel="noopener"
+          >
+            <img
+              src="https://i2-prod.manchestereveningnews.co.uk/sport/football/article27602598.ece/ALTERNATES/s1200c/0_Ruben-Dias.jpg"
+              alt=""
+            />
+            <h3> Ruben Dias</h3>
+            <p>Front End Developer</p>
+            <div className="div-profile">
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-512.png"
+                alt=""
+              />
+              <img
+                src="https://cdn.icon-icons.com/icons2/2248/PNG/512/linkedin_icon_135436.png"
+                alt=""
+              />
+              <img
+                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                alt=""
+              />
+            </div>
+          </a>
+        </div>
+
+        <div className="img-wrapper">
+          <a
+            href="https://altphotos.com/photo/sidewalk-terrace-of-a-blue-facade-cafe-312/"
+            target="_blank"
+            rel="noopener"
+          >
+             <img
+              src="https://i2-prod.manchestereveningnews.co.uk/sport/football/article27602598.ece/ALTERNATES/s1200c/0_Ruben-Dias.jpg"
+              alt=""
+            />
+            <h3> Ruben Dias</h3>
+            <p>Front End Developer</p>
+            <div className="div-profile">
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-512.png"
+                alt=""
+              />
+              <img
+                src="https://cdn.icon-icons.com/icons2/2248/PNG/512/linkedin_icon_135436.png"
+                alt=""
+              />
+              <img
+                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                alt=""
+              />
+            </div>
+          </a>
+        </div>
+
+        <div className="img-wrapper slower">
+          <a
+            href="https://altphotos.com/photo/paris-waterfront-at-sunset-1555/"
+            target="_blank"
+            rel="noopener"
+          >
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/paris-seine-boat.jpg"
+              alt=""
+            />
+
+            <h3> Darvin Nunez Martinez</h3>
+            <p>Back End Developer</p>
+            <div className="div-profile">
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-512.png"
+                alt=""
+              />
+              <img
+                src="https://cdn.icon-icons.com/icons2/2248/PNG/512/linkedin_icon_135436.png"
+                alt=""
+              />
+              <img
+                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                alt=""
+              />
+            </div>
+          </a>
+        </div>
+
+        <div className="img-wrapper faster1">
+          <a
+            href="https://altphotos.com/photo/old-man-leaning-over-the-barrier-looking-at-the-river-265/"
+            target="_blank"
+            rel="noopener"
+          >
+            <img
+              src="https://d3j2s6hdd6a7rg.cloudfront.net/v2/uploads/media/default/0002/22/thumb_121585_default_news_size_5.jpeg"
+              alt=""
+            />
+            <h3> Virjil van dijk</h3>
+            <p>UIUX Desiner</p>
+            <div className="div-profile">
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-512.png"
+                alt=""
+              />
+              <img
+                src="https://cdn.icon-icons.com/icons2/2248/PNG/512/linkedin_icon_135436.png"
+                alt=""
+              />
+              <img
+                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                alt=""
+              />
+            </div>
+          </a>
+        </div>
+
+        <div className="img-wrapper slower slower2">
+          <a
+            href="https://altphotos.com/photo/cafe-terrace-with-a-row-of-retro-tables-261/"
+            target="_blank"
+            rel="noopener"
+          >
+            <img
+              src="https://d3mm2s9r15iqcv.cloudfront.net/en/wp-content/uploads/2022/10/backend-developers-keep-everything-moving.jpeg"
+              alt=""
+            />
+            <h3> Darvin Nunez Martinez</h3>
+            <p>Back End Developer</p>
+            <div className="div-profile">
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-512.png"
+                alt=""
+              />
+              <img
+                src="https://cdn.icon-icons.com/icons2/2248/PNG/512/linkedin_icon_135436.png"
+                alt=""
+              />
+              <img
+                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                alt=""
+              />
+            </div>
+          </a>
+        </div>
+
+        <div className="img-wrapper">
+          <a
+            href="https://altphotos.com/photo/street-scene-with-pedestrians-and-dogs-318/"
+            target="_blank"
+            rel="noopener"
+          >
+            <img
+              src="https://history-biography.com/wp-content/uploads/2022/12/julian-alvarez.webp"
+              alt=""
+            />
+
+            <h3>Mohamad amin Keimasi</h3>
+            <p>Front End Developer</p>
+
+            <div className="div-profile">
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-512.png"
+                alt=""
+              />
+              <img
+                src="https://cdn.icon-icons.com/icons2/2248/PNG/512/linkedin_icon_135436.png"
+                alt=""
+              />
+              <img
+                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                alt=""
+              />
+            </div>
+          </a>
+        </div>
+
+        <div className="img-wrapper slower">
+          <a
+            href="https://altphotos.com/photo/tourist-barge-on-the-river-seine-near-notre-dame-266/"
+            target="_blank"
+            rel="noopener"
+          >
+            <img
+              src="https://d3mm2s9r15iqcv.cloudfront.net/en/wp-content/uploads/2022/10/backend-developers-keep-everything-moving.jpeg"
+              alt=""
+            />
+            <h3> Darvin Nunez Martinez</h3>
+            <p>Back End Developer</p>
+            <div className="div-profile">
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-512.png"
+                alt=""
+              />
+              <img
+                src="https://cdn.icon-icons.com/icons2/2248/PNG/512/linkedin_icon_135436.png"
+                alt=""
+              />
+              <img
+                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                alt=""
+              />
+            </div>
+          </a>
+        </div>
+
+        <div className="img-wrapper slower last">
+          <a
+            href="https://altphotos.com/photo/skulls-decoration-in-a-shop-window-331/"
+            target="_blank"
+            rel="noopener"
+          >
+            <img
+              src="https://history-biography.com/wp-content/uploads/2022/12/julian-alvarez.webp"
+              alt=""
+            />
+
+            <h3>Mohamad amin Keimasi</h3>
+            <p>Front End Developer</p>
+
+            <div className="div-profile">
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-512.png"
+                alt=""
+              />
+              <img
+                src="https://cdn.icon-icons.com/icons2/2248/PNG/512/linkedin_icon_135436.png"
+                alt=""
+              />
+              <img
+                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                alt=""
+              />
+            </div>
+          </a>
+        </div>
+      </div>
+      <p className="scroll-info">
+        <span className="icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            version="1.1"
+            x="0px"
+            y="0px"
+            viewBox="0 0 100 100"
+          >
+            <path d="M50,67.1c-0.6,0-1.2-0.2-1.8-0.7c-3.8-3.8-7.7-7.7-11.5-11.5c-2.3-2.3,1.2-5.8,3.5-3.5c2.5,2.5,4.9,4.9,7.4,7.4c0-13.7,0-27.4,0-41.2c0-0.6,0.2-1.2,0.5-1.5c0,0,0,0,0,0c0.4-0.6,1.1-1,2-0.9c13.7,0.3,26.4,7.2,33.5,19.1      C96.5,55.9,84.7,85,60.2,91.6C35.5,98.2,11.6,79.1,11.1,54c-0.1-3.2,4.9-3.2,5,0c0.3,13.8,8.4,26.4,21.3,31.5      c12.5,5,27.1,1.9,36.6-7.5c9.5-9.5,12.5-24.1,7.5-36.6c-4.8-12.1-16.3-20.1-29-21.2c0,12.8,0,25.5,0,38.3      c2.5-2.5,4.9-4.9,7.4-7.4c2.3-2.3,5.8,1.3,3.5,3.5c-3.9,3.9-7.8,7.8-11.8,11.8C51.2,66.9,50.6,67.1,50,67.1z" />
+          </svg>
+        </span>{" "}
+        <h5 className="header drop-in" id="tiggle" >
+        Try scrolling down
+        </h5>
+      </p>
+      <header>
+        <div  className="overflow-hidden">
+          <h3 className="header drop-in"> Todibo Team Members</h3>
+        </div>
+        <div className="overflow-hidden">
+          <p className="drop-in-2">
+          Team Developer is the 32-bit counterpart of the widely used 16-bit SQLWindows. Code developed with SQLWindows migrates easily to Team Developer
+          </p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Animation1;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './Animation1';
 import reportWebVitals from './reportWebVitals';
+import Animation1 from './Animation1';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Animation1/>
   </React.StrictMode>
 );
 
